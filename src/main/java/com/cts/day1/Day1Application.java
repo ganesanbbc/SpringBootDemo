55100765b325a264ad15c2fca12eecb438907b9a
GanesanNS <ganesan@arivuventures.com>
2017-05-26 20:58:33 +0530

    Optimize import

--- src/main/java/com/cts/day1/Day1Application.java
+++ src/main/java/com/cts/day1/Day1Application.java
@@ -1,11 +1,7 @@
 package com.cts.day1;
 
-import com.cts.day1.model.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 //@ComponentScan({"com.*.*", "com.cts.day1.*"})
