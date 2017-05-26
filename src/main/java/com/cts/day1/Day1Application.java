921efa66af162ce66087c53ce8f521a2a76bfd46
Ganesan Natarajan Shanmugam <ganesan.ns@cognizant.com>
2017-05-26 17:39:19 +0530

    Add first controller

--- src/main/java/com/cts/day1/Day1Application.java
+++ src/main/java/com/cts/day1/Day1Application.java
@@ -1,12 +1,17 @@
 package com.cts.day1;
 
+import com.cts.day1.model.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+//@ComponentScan({"com.*.*", "com.cts.day1.*"})
 public class Day1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Day1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Day1Application.class, args);
+    }
 }
---
+++ src/main/java/com/cts/day1/controllers/FirstController.java
@@ -0,0 +1,22 @@
+package com.cts.day1.controllers;
+
+
+import com.cts.day1.model.Product;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+
+    @RequestMapping(path = "/getProducts", method = RequestMethod.GET)
+    public @ResponseBody String getItems(){
+        return "success";
+    }
+
+
+
+
+}
