6296e0e86af0b9d0bad9e1140d0cdf317afacd94
GanesanNS <ganesan@arivuventures.com>
2017-05-27 10:10:54 +0530

    pass list of products as a response

--- src/main/java/com/cts/day1/controllers/FirstController.java
+++ src/main/java/com/cts/day1/controllers/FirstController.java
@@ -7,13 +7,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class FirstController {
 
     @RequestMapping(path = "/getProducts", method = RequestMethod.GET)
-    public @ResponseBody
-    Product getItems() {
-        return new Product("success");
+    public @ResponseBody List<Product> getItems() {
+        return getProducts();
+    }
+
+    private List<Product> getProducts() {
+        List<Product> products = new ArrayList();
+        products.add(new Product("success"));
+        return products;
     }
 
 
