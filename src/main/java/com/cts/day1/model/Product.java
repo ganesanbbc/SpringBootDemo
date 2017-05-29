abeb45b66ed1d3de35265dee67b2f092d539dc89
Ganesan Natarajan Shanmugam <ganesan.ns@cognizant.com>
2017-05-29 11:25:11 +0530

    Add product VO

---
+++ src/main/java/com/cts/day1/model/Product.java
@@ -0,0 +1,10 @@
+package com.cts.day1.model;
+
+public class Product {
+
+    private String productName;
+
+    public Product(String productName) {
+        this.productName = productName;
+    }
+}
