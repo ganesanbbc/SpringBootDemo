f9918b5e88c36830e74ed5b7290553bb6cbe82f1
GanesanNS <ganesan@arivuventures.com>
2017-06-08 21:12:55 +0530

    get Product by id

--- src/main/java/com/cts/day1/Day1Application.java
+++ src/main/java/com/cts/day1/Day1Application.java
@@ -1,14 +1,31 @@
 package com.cts.day1;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.cts.day1.dao.ProductRepository;
+import com.cts.day1.model.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 //@ComponentScan({"com.*.*", "com.cts.day1.*"})
 public class Day1Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Day1Application.class);
+
+
     public static void main(String[] args) {
         SpringApplication.run(Day1Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner setup(ProductRepository productRepository) {
+        return (args) -> {
+            productRepository.save(new Product("product1"));
+            productRepository.save(new Product("product2"));
+            logger.info("The sample data has been generated");
+        };
     }
 }
