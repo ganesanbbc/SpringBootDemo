0bc0f1efa873a5f2b7d31390e4a01737f5b48e32
Ganesan Natarajan Shanmugam <ganesan.ns@cognizant.com>
2017-06-09 09:49:51 +0530

    Implemented ControllerTest

---
+++ src/test/java/com/cts/day1/controllers/RestInterfaceControllerTest.java
@@ -0,0 +1,47 @@
+package com.cts.day1.controllers;
+
+
+import com.cts.day1.Day1Application;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Day1Application.class)
+@SpringBootTest
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RestInterfaceControllerTest {
+
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void thatGetProducts() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/getProducts").accept(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$", hasSize(2))).andDo(print());
+    }
+
+}
